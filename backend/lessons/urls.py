4d9b4c7242e967e8533228c107ddee9c2980ca00
justinecua <jcua.dev@gmail.com>
2025-11-11 16:22:56 +0800

    complete functionality for youtube, narration & video

--- backend/lessons/urls.py
+++ backend/lessons/urls.py
@@ -1,9 +1,13 @@
+from django.urls import path
 from rest_framework.routers import DefaultRouter
-from .views import TopicViewSet, MediaViewSet, GamificationProgressViewSet
+from .views import TopicViewSet, MediaViewSet, GamificationProgressViewSet, tts_voice_rss
 
 router = DefaultRouter()
-router.register(r"topics", TopicViewSet, basename="topic")
-router.register(r"media", MediaViewSet, basename="media")
-router.register(r"progress", GamificationProgressViewSet, basename="progress")
+router.register(r"topics", TopicViewSet)
+router.register(r"media", MediaViewSet)
+router.register(r"progress", GamificationProgressViewSet)
 
-urlpatterns = router.urls
+urlpatterns = [
+    *router.urls,
+    path("tts/", tts_voice_rss),
+]
